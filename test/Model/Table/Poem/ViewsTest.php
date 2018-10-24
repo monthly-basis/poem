d4e0a9aae9e849ba2d1bf976156bf56fb902ca2e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-24 22:33:40 +0000

    Write test for increment method

--- test/Model/Table/Poem/ViewsTest.php
+++ test/Model/Table/Poem/ViewsTest.php
@@ -11,6 +11,7 @@ class ViewsTest extends TableTestCase
         $this->dropTable('poem');
         $this->createTable('poem');
 
+        $this->poemTable = new PoemTable\Poem($this->getAdapter());
         $this->viewsTable = new PoemTable\Poem\Views($this->getAdapter());
     }
 
@@ -19,6 +20,25 @@ class ViewsTest extends TableTestCase
         $this->assertInstanceOf(
             PoemTable\Poem\Views::class,
             $this->viewsTable
+        );
+    }
+
+    public function testIncrementWherePoemId()
+    {
+        $this->assertSame(
+            0,
+            $this->viewsTable->incrementWherePoemId(1)
+        );
+
+        $this->poemTable->insert(
+            1,
+            'title',
+            'body'
+        );
+
+        $this->assertSame(
+            1,
+            $this->viewsTable->incrementWherePoemId(1)
         );
     }
 }
