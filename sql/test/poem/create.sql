1ac84380ec7a183a73ca901d8fe22e2e3b2d0515
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-03-14 19:19:19 +0000

    Add index on user_id column

--- sql/test/poem/create.sql
+++ sql/test/poem/create.sql
@@ -5,5 +5,6 @@ CREATE TABLE `poem` (
     `body` text,
     `views` int(10) unsigned NOT NULL DEFAULT '0',
     `created` datetime not null,
-    PRIMARY KEY (`poem_id`)
+    PRIMARY KEY (`poem_id`),
+    INDEX `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
